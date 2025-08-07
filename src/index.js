83240957f2f8002af1070b55f96d4703c20cc0ae
joy248 <manoharj1g@gmail.com>
2025-08-07 11:16:46 +0530

    Fix: env fallback added

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const os = require('os');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.text({ type: 'text/html' }));
 
